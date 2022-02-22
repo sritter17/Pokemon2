9159c598ebacd3101646638fd4f4c87a066ba429
sritter17 <sam.ritter2003@gmail.com>
2022-02-22 14:50:17 -0500

    More changes (2/22/22)

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -75,9 +75,38 @@ public class CollisionChecker {
 			if(gp.obj[i] != null) {
 				
 				entity.solidArea.x = entity.worldX + entity.solidArea.x;
+				entity.solidArea.y = entity.worldY + entity.solidArea.y;
+			
+				gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
+				gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
 				
-				
-				
+				switch(entity.direction) {
+				case "up":
+					entity.solidArea.y -= entity.intSpeed;
+					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						
+					}
+						
+					break;
+				case "down":
+					entity.solidArea.y += entity.intSpeed;
+					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						
+					}
+					break;
+				case "left":
+					entity.solidArea.x -= entity.intSpeed;
+					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						
+					}
+					break;
+				case "right":
+					entity.solidArea.y += entity.intSpeed;
+					if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						
+					}
+					break;
+				}
 			}
 		}
 		
